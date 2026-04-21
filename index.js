5fe4f65f66b27e82af227e72d4085166d1731a95
Shruti Singh Vishene <shrutivishene@gmail.com>
2026-04-21 22:30:43 +0530

    fix: updated CORS for deployed frontend

--- index.js
+++ index.js
@@ -34,11 +34,21 @@ connect()
   });
 
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL
+];
+
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
-
 
 
 
